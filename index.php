62cdee0d318cc41477d2b5885f7e6eecf59ee1bf
Francesco <franc.ns@gmail.com>
2020-01-07 14:18:44 +0100

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css">
+  <!-- JQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Handlebars -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.5.3/handlebars.min.js"></script>
+  <!-- JS -->
+  <script src="script.js"></script>
+
+  <!-- Handlebars Template -->
+  <script id="config_template" type="text/x-handlebars-template">
+  </script>
+
+
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
